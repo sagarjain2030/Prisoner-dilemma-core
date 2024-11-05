2bba216537ff1afe749fb35bb083b5b070f10209
Sagar Jain <sagarjain2030@gmail.com>
2024-11-05 08:38:16 +0530

    Basic working beast code

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,49 @@
-#include<iostream>
-#define ROUNDS 5
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/asio.hpp>
+#include <iostream>
 
-int main(int argc, char* argv[])
-{
-    std::cout<<"Prisoners Dilemma Problem"<<std::endl;
-    //Competition comp;
-    //for(int i = 0; i < ROUNDS; i++)
-    //{
-    //    comp.runCompetition();
-    //}    
-    //comp.getResults();
-    std::cout<<"End of Program"<<std::endl;
-    char ch = getchar();
-    std::cout << ch;
+namespace beast = boost::beast;
+namespace http = beast::http;
+namespace net = boost::asio;
+using tcp = net::ip::tcp;
+
+void handle_request(http::request<http::string_body> req, http::response<http::string_body>& res) {
+    res.version(req.version());
+    res.result(http::status::ok);
+    res.set(http::field::content_type, "application/json");
+    res.set(http::field::access_control_allow_origin, "*");
+    res.body() = R"({"message": "Hello from Boost.Beast!"})";
+    res.prepare_payload();
+}
+
+void start_server() {
+    net::io_context ioc;
+    tcp::acceptor acceptor(ioc, { tcp::v4(), 8080 });
+
+    for (;;) {
+        tcp::socket socket(ioc);
+        acceptor.accept(socket);
+
+        beast::flat_buffer buffer;
+        http::request<http::string_body> req;
+        http::read(socket, buffer, req);
+
+        http::response<http::string_body> res;
+        handle_request(req, res);
+
+        http::write(socket, res);
+        socket.shutdown(tcp::socket::shutdown_send);
+    }
+}
+
+int main() {
+    try {
+        start_server();
+    }
+    catch (std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
     return 0;
 }
